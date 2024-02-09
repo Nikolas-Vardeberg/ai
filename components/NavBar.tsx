58b751689986a24245258ce1d8dc72a6992d5a35
niklusrook <post@nikolas.no>
2024-02-09 13:58:50 +0100

    shadn-ui installasjon + connection to db

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+
+
+const NavBar = () => {
+    return(
+        <div>
+            navbar
+        </div>
+    )
+}
+
+export default NavBar;
